46c6fa379efc90da9f56166a8e94b21580a57582
vitalytva <vitalytva@yandex.ru>
2022-11-20 18:05:33 +0400

    extensions

--- src/MetaCore/Utils.cs
+++ src/MetaCore/Utils.cs
@@ -3,5 +3,18 @@
         public static Stream GetStream(Type typeFromAssembly, string filename) {
             return typeFromAssembly.Assembly.GetManifestResourceStream(typeFromAssembly.Assembly.GetName().Name + ".Assets." + filename);
         }
+        public static TR Trasform<TS, TR>(this TS value, Func<TS, TR> selector) => selector(value);
+        public static IEnumerable<T> Yield<T>(this T value) { 
+            yield return value;
+        }
+        public static IEnumerable<T> Yield<T>(this (T, T) value) {
+            yield return value.Item1;
+            yield return value.Item2;
+        }
+        public static IEnumerable<T> Yield<T>(this (T, T, T) value) {
+            yield return value.Item1;
+            yield return value.Item2;
+            yield return value.Item3;
+        }
     }
 }
